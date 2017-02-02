bbcd4d3f7bffe5511330802fde999233f2c75627
yellowcd <yellowd91@gmail.com>
2017-02-02 16:17:09 +0900

    update util file from sclee

--- LiveVessel/util.h
+++ LiveVessel/util.h
@@ -11,7 +11,8 @@ util.h
 
 //extern FILE *log_stream;
 
-void InitLogStream(char *path, bool b_reinit=false);
+void InitLogStream(char *path, bool b_reinit = false);
 void WriteLog(char *_file, int _line, char *_ftn = NULL, char *log = NULL);
-//void WriteLog(char *_file, int _line, char *log=NULL);
+void WriteLog(char *log);
+
 void CloseLogStream();
